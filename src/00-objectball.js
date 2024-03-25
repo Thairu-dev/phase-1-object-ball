1da7cdfb4d825261d7766dd925d775d5cc0bd3cd
Joseph_Thairu <joejoement@gmail.com>
2024-03-25 14:28:33 +0300

    completed

---
+++ src/00-objectball.js
@@ -0,0 +1,25 @@
+const gameObjects = {
+    home:{
+        teameName:"Brooklyn Nets",
+        colors :[Turquoise, Purple],
+        players:{
+            "Alan Anderson" : [0, 16, 22, 12, 12, 3, 1, 1],
+            "Reggie Evans": [30, 14, 12, 12, 12, 12, 12, 7],
+            "Brook Lopez":  [11, 17, 17, 19, 10, 3, 1, 15],
+            "Mason Plumlee": [1, 19, 26, 12, 6, 3, 8, 5],
+            "Jason Terry":   [31, 15, 19, 2, 2, 4, 11, 1],
+            
+        
+        }
+
+    },
+    away:{
+        teameName:"Charlotte Hornets"
+        colors:
+        players:{
+            names:
+        }
+    }
+
+}
+console.log(gameObjects)
